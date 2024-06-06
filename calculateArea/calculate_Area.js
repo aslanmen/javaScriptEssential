98c081e968f01335ee783ea29121d2bf31e62a9b
aslanmen <aaslann.111@gmail.com>
2024-06-06 08:43:18 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,7 +6,25 @@ function calculateArea(){
     width=document.getElementById("width").value
     
     let area=length*width
-
     document.getElementById("result").innerText=`The area of the rectangle is: ${area}`
+    
+}
 
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker(){
+    g1=(document.getElementById("grocery1").value)
+    g2=(document.getElementById("grocery2").value)
+    g3=(document.getElementById("grocery3").value)
+    
+    grocery1 = g1 ? parseFloat(g1) : 0;
+    grocery2 = g2 ? parseFloat(g2) : 0;
+    grocery3 = g3 ? parseFloat(g3) : 0;
+
+    let total=grocery1+grocery2+grocery3
+    
+    document.getElementById("total").innerText=`The total amount is:$ ${total}`
+    
 }
